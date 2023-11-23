2175c249e2aa22dfbb8f35b689d66d866f058232
Andreksus <baryshev_2002@inbox.ru>
2023-11-23 11:26:56 +0600

    Initial Commit

---
+++ DoctorLab/DoctorLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorLabApp.swift
+//  DoctorLab
+//
+//  Created by student on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DoctorLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
